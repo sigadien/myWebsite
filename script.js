8610a42439311b0dd1511c3f461d38b459304058
sigadien <129775825+sigadien@users.noreply.github.com>
2023-04-04 20:30:12 +0200

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', e => {
-    document.body.append("Thanks for seeing by!");
-
-    // creat text with random color when reloading website
-    let el = document.querySelector("#world");
-    el.style.color= "#"
-    + Math.floor(Math.random()*16777215).toString(16);
- });
